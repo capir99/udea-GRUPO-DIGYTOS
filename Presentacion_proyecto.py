07e79cc3522ffceb0a754576018bd3c322168cbb
capir99 <41745818+capir99@users.noreply.github.com>
2021-09-15 20:10:08 -0500

    Presentación_proyecto
    Presentación_proyecto en formato py

---
+++ Presentacion_proyecto.py
@@ -0,0 +1,24 @@
+print("\nProyecto ciclo 3 -Dygitos ")
+
+print("\nRepositorio GitHub ciclo 3 grupos integrantes : Aicardo Emmanuel Suárez Balaguera, Jean Ruiz, Samuel Eliecer Niño Quintero, Camilo Andrés Pinto, Jhoan Felipe Cortés")
+
+print("\nDescripción proyecto aplicación web ventas tienda Digytos")
+print("\nAlcance: Realizar el diseño,implementación, pruebas y despliegue del sistema de ventas de la tienda de Digytos (tienda online)")
+
+print("\n")
+print("\nMódulos que lo componen:")
+print("\n")
+print("\n*Módulo administrador de ventas:")
+print("\nPermite ingresar o registrar los pedidos realizados por cada uno de los clientes. Deberá tener un identificador único de venta, almacenar el valor total de la venta y la descripción detallada de la misma y tener una fecha inicial y una fecha futura de pago. Además, deberá contar con un encargado de gestionar dicha venta (responsable).")
+
+print("\n*Módulo para registrar el estado de la venta:")
+print("\nDescribir...")
+
+print("\n*Gestión de vendedores:")
+print("\nDescribir...")
+
+print("\n*Gestión de ingreso al sistema de información:")
+print("\nDescribir...")
+
+print("\n*Gestión de usuarios y roles:")
+print("\nDescribir...")
